bfc04b3df64b1d7200fe50c0d456a3fca4715398
Joakim Lazakis <ilazakis@gmail.com>
2016-11-26 11:57:08 +0000

    About layout: Added Github logo (missing link)

--- app/src/main/java/com/cocoapatterns/herocam/herocam/about/AboutFragment.java
+++ app/src/main/java/com/cocoapatterns/herocam/herocam/about/AboutFragment.java
@@ -6,15 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.cocoapatterns.herocam.herocam.R;
 
 public class AboutFragment extends Fragment {
-
-    // Default constructor
-    public AboutFragment() {}
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
