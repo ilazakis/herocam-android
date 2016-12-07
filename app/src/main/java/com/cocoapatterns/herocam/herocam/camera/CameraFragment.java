a22fb4e83714aac0b597548baaff9d2e58c05d3d
Joakim Lazakis <ilazakis@gmail.com>
2016-12-07 20:45:46 +0000

    Camera permission handling.

--- app/src/main/java/com/cocoapatterns/herocam/herocam/camera/CameraFragment.java
+++ app/src/main/java/com/cocoapatterns/herocam/herocam/camera/CameraFragment.java
@@ -1,7 +1,13 @@
 package com.cocoapatterns.herocam.herocam.camera;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,13 +24,59 @@ public class CameraFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
 
-        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        cameraPreview = new CameraPreview(this.getContext(), CameraService.getCamera());
+        // If we have permission to use the CAMERA, open the camera.
+        if (hasCameraPermission()) {
+            openCamera(view);
+        }
+        // Otherwise ask for permission.
+        else {
+            askForCameraPermission();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera(getView());
+                }
+        }
+
+    }
+
+    private void openCamera(View view) {
+        // Try to get a Camera to work with and proceed if we succeeded.
+        Camera camera = getCamera();
+        if (camera != null) { updateCameraPreview(camera, view); }
+    }
+
+    private void updateCameraPreview(Camera camera, View view) {
+        cameraPreview = new CameraPreview(this.getContext(), camera);
         FrameLayout preview = (FrameLayout) view.findViewById(R.id.camera_preview);
         preview.addView(cameraPreview);
+    }
 
-        return view;
+    private boolean hasCameraPermission() {
+        int cameraPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA);
+        return cameraPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void askForCameraPermission() {
+        requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
+    }
+
+    private Camera getCamera() {
+        return CameraService.getCamera();
     }
 }
