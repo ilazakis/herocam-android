bc3875b7771980afdab682d20a3ddda39ab0ff95
Joakim Lazakis <ilazakis@gmail.com>
2016-12-10 11:59:45 +0000

    Espresso tests import statement cleanup.

--- app/src/androidTest/java/com/cocoapatterns/herocam/herocam/MainActivityTests.java
+++ app/src/androidTest/java/com/cocoapatterns/herocam/herocam/MainActivityTests.java
@@ -5,13 +5,11 @@ import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.view.ViewPager;
-import android.widget.ImageView;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.getIdlingResources;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
@@ -20,8 +18,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDis
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.anyOf;
-import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTests {
